12e2d854139539b7bc8bf45a872b9d8dcf92affb
wnbaltona <wniziolek@baltona.pl>
2026-07-29 10:08:42 +0200

    Add dropdown menu functionality

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const menuButton =
+document.getElementById("menuButton");
+
+
+const dropdown =
+document.getElementById("dropdown");
+
+
+menuButton.onclick = ()=>{
+
+dropdown.classList.toggle("show");
+
+};
