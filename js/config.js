1e3f2a5194fd98e4210d660a5845b7d38ab91930
wnbaltona <wniziolek@baltona.pl>
2026-07-29 14:03:49 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+const APP_LOCATIONS = [
+
+"OKĘCIE",
+"RADOM",
+"MODLIN",
+"BYDGOSZCZ",
+"KRAKÓW",
+"POZNAŃ",
+"WROCŁAW",
+"ŚWINOUJŚCIE",
+"GDAŃSK",
+"GDYNIA",
+"ZIELONA GÓRA",
+"RZESZÓW",
+"FRANCJA",
+"KATOWICE"
+
+];
