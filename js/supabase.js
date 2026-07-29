72299f4fe6f35373b8f424678dd2a092718ef1ed
wnbaltona <wniziolek@baltona.pl>
2026-07-29 14:35:40 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,8 +4,7 @@ const SUPABASE_URL = "https://bopilhuqrbagngnhdpyj.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvcGlsaHVxcmJhZ25nbmhkcHlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyOTcyOTUsImV4cCI6MjEwMDg3MzI5NX0.NsOeMJTy6CMd8cPy_sw1jnakrBKxNQejFpEkN-2gz-4";
 
 
-const supabaseClient =
-supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
